6099ed6e5234ea9adb6712de9a344975a57c2390
Githron <allewnor@gmail.com>
2026-02-08 21:54:48 +0000

    updated

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,15 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // This will allow us to parse JSON data from the request body
 
 app.use("/api/products", productRoutes)
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000 hello");
+  console.log(`Server started at http://localhost:${PORT} hello`);
 });
 
 // lacidaromelo_db_user GbSWuc56x2ts5dcs
